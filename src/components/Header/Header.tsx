bd9f6fe907e405a2570dc318afe6faf1d2c774ff
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-05-15 14:40:49 +0300

    Added images: github.svg, instagram.svg, linkedin.svg, added context to Header.tsx and styles for it, react router dom

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import s from 'components/Header/Header.module.css'
+import github from 'assets/github.svg'
+import instagram from 'assets/instagram.svg'
+import linkedin from 'assets/linkedin.svg'
+import {Link} from "react-router-dom";
+
 export const Header = () => {
     return (
         <header className={s.header}>
             <div className={s.container}>
-               <p>Здесь что</p>
+                <div className={s.burger}>
+                    здесь будет бургер меню
+                </div>
+                <div className={s.content}>
+                    <div>
+                        <Link to={'https://github.com/Vladimirkasperovich?tab=repositories'}>
+                            <img className={s.github_img} src={github} alt=""/>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'https://www.linkedin.com/in/vladimir-kasperovich-38b157243/'}>
+                            <img className={s.linkedin_img} src={linkedin}/>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={'https://www.instagram.com/vova_kasper/'}>
+                            <img className={s.instagram_img} src={instagram}/>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </header>
     );
